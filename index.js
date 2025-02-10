01a6264e5f8fa453a23a557ceba50d4e582957ac
olga-rudacivs <olga.rudacivs1991@gmail.com>
2025-02-10 22:55:43 +0500

    init

---
+++ index.js
@@ -0,0 +1,72 @@
+const TelegramApi = require("node-telegram-bot-api");
+
+const {gameOptions, againOptions} = require('./options.js')
+
+const token = "7993623557:AAFf1PxaSjm83sPtDpN5vBt8xi7fgXF11ZA";
+
+const bot = new TelegramApi(token, { polling: true });
+
+const chats = {};
+
+const startGame = async (chatId) => {
+  await bot.sendMessage(
+    chatId,
+    ` я загадаю цифру, а ты отгадывай(цифра от 0 до 9)`
+  );
+  const randomNum = Math.floor(Math.random() * 10);
+  chats[chatId] = randomNum;
+  await bot.sendMessage(chatId, "отгадывай", gameOptions);
+};
+
+function start() {
+  bot.setMyCommands([
+    { command: "/start", description: "приветствие" },
+    { command: "/info", description: "информация" },
+    { command: "/game", description: "игра" },
+  ]);
+
+  bot.on("message", async (msg) => {
+    const text = msg.text;
+    const chatId = msg.chat.id;
+
+    if (text === "/start") {
+      await bot.sendMessage(chatId, `hello pipa`);
+      return bot.sendMessage(
+        chatId,
+        "https://tlgrm.ru/_/stickers/4dd/300/4dd300fd-0a89-3f3d-ac53-8ec93976495e/3.jpg"
+      );
+    }
+    if (text === "/info") {
+      return bot.sendMessage(chatId, `${msg.from.first_name}, ti pipa`);
+    }
+    if (text === "/game") {
+     return startGame(chatId) }
+    return bot.sendMessage(chatId, "я не понял ниче");
+  });
+
+  bot.on("callback_query", async (msg) => {
+    const data = msg.data;
+    const chatId = msg.message.chat.id;
+
+  if(data === '/again'){
+return startGame(chatId)
+    }
+    if (data === chats[chatId]) {
+      return bot.sendMessage(
+        chatId,
+        `ты угадал! это ${chats[chatId]}!!!`,
+        againOptions
+      );
+    } else {
+      return bot.sendMessage(
+        chatId,
+        `не то, я загадал ${chats[chatId]}`,
+        againOptions
+      );
+    }
+
+    console.log(msg);
+  });
+}
+
+start();
